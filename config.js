caeb3596f6300207a0d0c5c23614066587b19a4a
etsu1617 <e--yoshikata@outlook.jp>
2026-04-20 09:02:21 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 
 const CONFIG = {
   SUPABASE_URL: 'https://idydpopwmdzepinocgxy.supabase.co',
-  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlkeWRwb3B3bWR6ZXBpbm9jZ3h5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY1MzMzMzAsImV4cCI6MjA5MjEwOTMzMH0.ymlzcaro-uqRvqdVeLlUlOaK40xKByxwK54w6mSilKI',
+  SUPABASE_ANON_KEY: 'sb_publishable_JDDDlb0nsswd...',
   BACKEND_URL: 'https://shiftboard-oq86.onrender.com',
 };
